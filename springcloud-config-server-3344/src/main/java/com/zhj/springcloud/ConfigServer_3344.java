367fb59fa5980c0b6c38b80e682e5c43025cc4d8
zhuhuijie <1406676103@qq.com>
2020-07-20 19:24:19 +0800

    springCloud Demo

--- springcloud-config-server-3344/src/main/java/com/zhj/springcloud/ConfigServer_3344.java
+++ springcloud-config-server-3344/src/main/java/com/zhj/springcloud/ConfigServer_3344.java
@@ -1,10 +1,21 @@
 package com.zhj.springcloud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
 /**
  * @PackageName:com.zhj.springcloud
  * @ClassName:ConfigServer_3344
  * @auter: 朱慧杰
  * @date:2020/7/20 0020 19:11
  **/
+@SpringBootApplication
+@EnableConfigServer
 public class ConfigServer_3344 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServer_3344.class, args);
+    }
+
 }
